9b9a565a7e53c893447c47c54ea134f743f5cb3e
VishnuB01 <baasvishnu@gmail.com>
2026-02-16 21:29:34 +0530

    Test file code added

--- test/radar_chart_plus_test.dart
+++ test/radar_chart_plus_test.dart
@@ -1,19 +1,46 @@
 import 'dart:ui';
 
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:radar_chart_plus/radar_chart_plus.dart';
 
 void main() {
   test('RadarChartPlus can be created', () {
-    RadarChartPlus(
-      dotColor: Color(0xFF8072F3),
-      chartBorderColor: Color(0xFF8072F3),
-      chartFillColor: Color(0x668072F3),
-      ticks: [2, 4, 6, 8, 10],
-      labels: ['AA', 'BB', 'CC', 'DD', 'EE', 'FF'],
-      data: [3, 2, 5, 6, 5, 9],
-
+    SizedBox(
+      width: 400,
+      height: 400,
+      child: RadarChartPlus(
+        ticks: [2, 4, 6, 8, 10],
+        labels: [
+          'Speed',
+          'Power',
+          'Defense',
+          'Agility',
+          'Intelligence',
+          'Stamina',
+        ],
+        dataSets: [
+          RadarDataSet(
+            data: [1, 2, 3, 4, 5, 6],
+            borderColor: const Color(0xFF8072F3),
+            fillColor: const Color(0x668072F3),
+            label: 'Player A',
+          ),
+          RadarDataSet(
+            data: [6, 7, 8, 9, 10, 1],
+            borderColor: const Color(0xFFFF6B6B),
+            fillColor: const Color(0x66FF6B6B),
+            label: 'Player B',
+          ),
+          RadarDataSet(
+            data: [6, 5, 4, 2, 2, 3],
+            borderColor: const Color(0xFF10FF00),
+            fillColor: const Color(0x6210FF00),
+            label: 'Player A',
+          ),
+        ],
+      ),
     );
   });
 }
